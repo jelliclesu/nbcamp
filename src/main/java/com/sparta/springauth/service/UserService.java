b62da8f888778e7dd51b58d9f62ff870d5ac5f1c
jelliclesu <jelliclesu@naver.com>
2024-10-21 20:50:19 +0900

    feat: Spring Security 로그인

--- src/main/java/com/sparta/springauth/service/UserService.java
+++ src/main/java/com/sparta/springauth/service/UserService.java
@@ -73,11 +73,9 @@ public class UserService {
         if (!passwordEncoder.matches(password, user.getPassword())) {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
-        System.out.println("password = " + password);
 
         // JWT 생성 및 쿠키에 저장 후 Response 객체에 추가
         String token = jwtUtil.createToken(user.getUsername(), user.getRole());
         jwtUtil.addJwtToCookie(token, res);
-
     }
 }
