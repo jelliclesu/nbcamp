4e67ff094df1a94113a4255f5e618df7cc35194f
jelliclesu <jelliclesu@naver.com>
2024-10-24 15:17:00 +0900

    feat: 폴더 별 관심 상품 조회 구현

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -8,4 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAllByUser(User user, Pageable pageable);
+
+    // select * from product p left join product_folder pf on p.id = pf.product_id where p.user_id = 1 and pf.folder_id = 1 order by p.id limit 0, 10;
+    // ByUser: p.user_id = 1, ProductFolderList_folderId: pf.folder_id = 1
+    Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
 }
